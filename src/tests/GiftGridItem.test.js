3ed47264445513914c89526542cc51a609476610
Jesus Martín <jesus.martinsamano@gmail.com>
2021-08-31 18:04:22 -0500

    Pruebas unitarias

---
+++ src/tests/GiftGridItem.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import '@testing-library/jest-dom'
+
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import { GiftGridItem } from '../components/GiftGridItem';
+
+
+
+describe('Pruebas en componente GiftGridItem', () => {
+
+    const title = 'Un título';
+    const url = 'https://localhost.imagen.jpg'
+    const wrapper = shallow( <GiftGridItem  title={ title } url={ url }/> );
+
+    test('Debe de mostrar el componente correctamente ', () => {
+        expect(toJson( wrapper )).toMatchSnapshot();  
+    });
+
+    test('debe de tener un parrafo con el title', () => {
+        const p =  wrapper.find( 'p' );
+        expect( p.text().trim() ).toBe( title );        
+    });
+
+    test('Debe de tener la imagen igual al url y alt de los props ', () => {
+        const img =  wrapper.find( 'img' );
+        expect( img.prop('src') ).toBe( url );
+        expect( img.prop('alt') ).toBe( title );
+    });
+
+    test('Debe de tener animated__fadeIn ', () => {
+        console.clear()
+        const div = wrapper.find('div');
+        console.log(div.prop('className'));
+    })
+    
+    
+    
+})
